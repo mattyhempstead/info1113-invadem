e973f0dba158c5245e6af6b5900c1f3db261fa9e
Matty <matty.hempstead@gmail.com>
2019-10-27 18:06:46 +1100

    Add assignment scaffold and docs

---
+++ Scaffold/src/main/java/invadem/App.java
@@ -0,0 +1,27 @@
+package invadem;
+
+import processing.core.PApplet;
+
+public class App extends PApplet {
+
+    public App() {
+        //Set up your objects
+    }
+
+    public void setup() {
+        frameRate(60);
+    }
+
+    public void settings() {
+        size(640, 480);
+    }
+
+    public void draw() { 
+        //Main Game Loop
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("invadem.App");
+    }
+
+}
